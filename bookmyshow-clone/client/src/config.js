9cab5134607dde4caa9f23597c43f0942000e9b1
Harsh Kumar <gharshgupta490@gmail.com>
2026-03-05 19:06:36 +0530

    Book My Show Clone Edited Successfully

--- bookmyshow-clone/client/src/config.js
+++ bookmyshow-clone/client/src/config.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://bookmyshow-backend.onrender.com"
+    ? "https://bookmyshow-clone-2knh.onrender.com"
     : "http://localhost:5000";
